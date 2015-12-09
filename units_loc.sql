c9c8078d47ad0d8e2afa7df32b73947c076f49b6
vadmin <vadmin>
2015-12-09 16:42:28 +0800

    updated sql

--- units_loc.sql
+++ units_loc.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2015 at 09:12 AM
+-- Generation Time: Dec 09, 2015 at 09:42 AM
 -- Server version: 10.0.17-MariaDB
 -- PHP Version: 5.6.14
 
@@ -45,8 +45,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `sku`, `name`, `monitora`, `mrp`, `description`, `packing`, `image`, `category`, `monitorb`, `status`) VALUES
-(953, 0, 'Asus 354', 'Samsung 6563', 0, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet assumenda consectetur dicta et facere fugit impedit iure maiores nam numquam odio officiis, quidem sint vitae? Cupiditate nemo odio qui.', 'no reasons', '', 0, 'Lg 359', 'Active'),
-(954, 0, 'dsfgsdfgsdfgsd', 'Dell', 0, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet assumenda consectetur dicta et facere fugit impedit iure maiores nam numquam odio officiis, quidem sint vitae? Cupiditate nemo odio qui.', 'no reasons', '', 0, 'AOC', 'Active');
+(951, 0, 'Asus 64', 'AOC 123', 0, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi commodi consectetur eveniet fugiat magni molestias natus nihil quae repudiandae similique. Aliquam, excepturi, voluptatem. Ab aspernatur in perferendis quam, quia vero.\nLorem ipsum dolor sit amet, consectetur adipisicing elit. Animi commodi consectetur eveniet fugiat magni molestias natus nihil quae repudiandae similique. Aliquam, excepturi, voluptatem. Ab aspernatur in perferendis quam, quia vero.', 'TRANSFER WORK', '', 0, 'SAMSUNG', 'Active'),
+(952, 0, 'Dell', 'Dell 1920x1080', 0, '', '-', '', 0, 'ACER 1920x1080', 'Active');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +66,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=955;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=953;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
